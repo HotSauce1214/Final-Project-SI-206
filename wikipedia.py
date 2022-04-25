9efbd608647fe5790e6ceda108e43c0c1f06718f
nscheele <nscheele@umich.edu>
2022-04-25 15:08:55 -0400

    Commit 11

--- wikipedia.py
+++ wikipedia.py
@@ -31,12 +31,4 @@ def setUpDatabase(db_name):
 
 
 def main():
-    def get_grammy_winners(filename):
-        with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), filename), 'r') as f:
-            r = f.read()
-        soup = BeautifulSoup(r, 'html.parser')
-        r = requests.get('https://en.wikipedia.org/wiki/Grammy_Award_for_Song_of_the_Year')
-        soup = BeautifulSoup(r, 'html.parser')
-
-        #this isn't right, it should be something more specific I think
-        tags = soup.find_all("table", class_ = "wikitable sortable jquery-tablesorter")
+    pass
